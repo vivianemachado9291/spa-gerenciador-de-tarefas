404c7128267114cadbbb0d7af277d3997735cbce
Viviane Machado <vi.ma.silvaa9291@gmail.com>
2025-10-29 19:04:26 -0300

    Refactor route management in router.js
    Refactor route handling by introducing renderRoute function and removing the old routes object.

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,23 +1,18 @@
 import { Templates } from './templates.js';
 import { mount } from './dom.js';
 import { store } from './store.js';
+import { hydrateCadastro, hydrateTarefas, hydratePerfil } from './hydrate.js';
 
-
-export const routes = {
-'/home': () => Templates.home(),
-'/cadastro': () => Templates.cadastro(),
-'/tarefas': () => Templates.tarefas(),
-'/perfil': () => Templates.perfil(store.get('profile', {})),
-'/sobre': () => Templates.sobre(),
-};
-
-
-export function resolveRoute(path) {
-return routes[path] ? path : '/home';
-}
-
-
-export function renderRoute($root, path) {
-const html = (routes[path] || routes['/home'])();
-mount($root, html);
+export function renderRoute($root, path){
+  let html='', data=null;
+  switch(path){
+    case '/cadastro': html=Templates.cadastro(); break;
+    case '/tarefas': html=Templates.tarefas(); break;
+    case '/perfil': data=store.get('profile'); html=Templates.perfil(data); break;
+    default: html=Templates.home();
+  }
+  mount($root, html);
+  if(path==='/cadastro') hydrateCadastro(document);
+  if(path==='/tarefas') hydrateTarefas(document);
+  if(path==='/perfil') hydratePerfil(document);
 }
