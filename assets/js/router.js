0bbf9554c1b7eab47f7a84d20618946fa3800779
Viviane Machado <vi.ma.silvaa9291@gmail.com>
2025-10-29 19:38:28 -0300

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,20 +1,29 @@
+// router.js
 import { Templates } from "./templates.js";
 import { mount, $ } from "./dom.js";
+import { store } from "./store.js";
+import { hydrateCadastro, hydrateTarefas, hydratePerfil } from "./hydrate.js";
 
-export function router() {
-  const routes = {
-    "#/home": Templates.home,
-    "#/cadastro": Templates.cadastro,
-    "#/tarefas": Templates.tarefas,
-    "#/perfil": Templates.perfil
-  };
-
-  function resolve() {
-    const hash = window.location.hash || "#/home";
-    const page = routes[hash] ? routes[hash]() : Templates.home();
-    mount($("#app"), page);
+function render(path){
+  let html = "";
+  switch (path) {
+    case "/cadastro": html = Templates.cadastro(); break;
+    case "/tarefas":  html = Templates.tarefas();  break;
+    case "/perfil":   html = Templates.perfil(store.get("profile", {})); break;
+    default:          html = Templates.home();
   }
+  mount($("#app"), html);
 
+  if (path === "/cadastro") hydrateCadastro(document);
+  if (path === "/tarefas")  hydrateTarefas(document);
+  if (path === "/perfil")   hydratePerfil(document);
+}
+
+export function startRouter(){
+  const resolve = () => {
+    const path = (location.hash || "#/home").replace("#", "");
+    render(path);
+  };
   window.addEventListener("hashchange", resolve);
   window.addEventListener("load", resolve);
 }
